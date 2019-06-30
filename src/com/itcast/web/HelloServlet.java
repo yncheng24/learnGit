8e09df75706ef828399f253b9e7e67163f14d3d4
yncheng <1433357652@qq.com>
2019-06-30 19:19:24 +0800

    init

--- src/com/itcast/web/HelloServlet.java
+++ src/com/itcast/web/HelloServlet.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 
 public class HelloServlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        response.getWriter().write("hello  yncheng");
+        response.getWriter().write("hello  yncheng 11111");
+        int a;
+        int b;
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
