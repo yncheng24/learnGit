35335b84a82d8651979ceee71ee8b13d6e41c2aa
yncheng <1433357652@qq.com>
2019-06-30 19:03:15 +0800

    init

---
+++ src/com/itcast/web/HelloServlet.java
@@ -0,0 +1,13 @@
+package com.itcast.web;
+
+import java.io.IOException;
+
+public class HelloServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.getWriter().write("hello  yncheng");
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        doPost(request,response);
+    }
+}
